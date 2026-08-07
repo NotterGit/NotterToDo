676e943b85b00fb190ab8f92dbe215e772e342aa
keeniGithub <maratkanovartem89@mail.ru>
2026-08-07 16:34:49 +0300

    add lists

---
+++ src/actions/create-list/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod"
+
+export const CreateList = z.object({
+    title: z.string({
+        message: "Title is required"
+    }).min(3, {
+        message: "Title is too short"
+    }),
+    boardId: z.string()
+})
