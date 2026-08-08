6fb0ed6d03a3572ada64f32a974b0a1f9d649998
keeniGithub <maratkanovartem89@mail.ru>
2026-08-08 22:19:28 +0300

    add list and card drag&drop

--- src/app/(dashboard)/_components/card-item.tsx
+++ src/app/(dashboard)/_components/card-item.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Card } from "@prisma/client"
+import { Draggable } from "@hello-pangea/dnd"
 
 interface CardItemProps {
     data: Card
@@ -11,8 +12,18 @@ export function CardItem({
     data, index
 }: CardItemProps) {
     return (
-        <div className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm" role="button">
-            {data.title}
-        </div>
+        <Draggable draggableId={data.id} index={index}>
+            {(provided) => (
+                <div 
+                    className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm" 
+                    role="button"
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                >
+                    {data.title}
+                </div>
+            )}
+        </Draggable>
     )
 }
